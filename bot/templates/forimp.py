4feae7394ab1e671d4e32d7273c6659f0de9a7ec
wilhelm43 <105068971+WWX-Team@users.noreply.github.com>
2024-05-16 22:03:33 +0200

    Create forimp.py
    [new]

---
+++ bot/templates/forimp.py
@@ -0,0 +1,14 @@
+"""
+
+Fonctions et classes rimitives pouvant être utilisées par le bot.
+
+"""
+
+class user
+
+  def __init__(id : int):
+    self.id = id
+    # https://discord.com/api/v9/users/
+    # https://stackoverflow.com/questions/64933979/discord-get-user-by-id
+
+def cmd_vérifier(utilisateur)
