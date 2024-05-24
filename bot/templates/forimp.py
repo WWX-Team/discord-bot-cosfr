cabe7840a4a3d5ea1c6f628c2d54917d0b7eb818
limagirare <oriolelcastagno@gmail.com>
2024-05-24 19:33:49 +0200

    Update forimp.py

--- bot/templates/forimp.py
+++ bot/templates/forimp.py
@@ -1,10 +1,25 @@
+import discord, os
+from discord.ext import commands
+
+
+
+my_secret = os.environ['bot_token']
+
+token = os.getenv("bot_token")
+bot_name = "My First Bot"
+cmd_prefix = "/"
+mod_role = "Mod Role Name"
+
+client = commands.Bot(command_prefix = cmd_prefix, intents = discord.Intents.all())
+
+
 """
 
 Fonctions et classes rimitives pouvant être utilisées par le bot.
 
 """
 
-class user
+class user:
 
   def __init__(id : int):
     self.id = id
